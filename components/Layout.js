faf3ddee35c614694acd8945a9e0e742c81130d1
Antonio Moccia <moccia.ant@gmail.com>
2022-09-30 08:28:05 +0200

    aggiunto lerp su smartphone e tablet

--- components/Layout.js
+++ components/Layout.js
@@ -1,31 +1,29 @@
-import React, {useRef, useEffect, useState } from 'react'
-import Navigation from './Navigation'
-import {LocomotiveScrollProvider} from 'react-locomotive-scroll'
-import {useRouter} from 'next/router'
+import React, { useRef, useEffect, useState } from "react";
+import Navigation from "./Navigation";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRouter } from "next/router";
 
-function Layout({children}) {
-
-    const containerRef = useRef(null)
-    const { asPath } = useRouter()
-    return (
-        <>
-
+function Layout({ children }) {
+  const containerRef = useRef(null);
+  const { asPath } = useRouter();
+  return (
+    <>
       <LocomotiveScrollProvider
         options={{
           smooth: true,
-          lerp:.07,
-          multiplier:1,
+          lerp: 0.07,
+          multiplier: 1,
           scrollFromAnywhere: true,
           reloadOnContextChange: true,
-          tablet: { smooth: true  },
-          smartphone: { smooth: true },
+          tablet: { smooth: true, lerp: 0.07 },
+          smartphone: { smooth: true, lerp: 0.07 },
         }}
         watch={["router.asPath"]}
         location={asPath}
         containerRef={containerRef}
         onLocationChange={(scroll) => {
           scroll.scrollTo(0, { duration: 0, disableLerp: true });
-//          document.querySelectorAll(".c-scrollbar")[0].style.display = "none";
+          //          document.querySelectorAll(".c-scrollbar")[0].style.display = "none";
           //        scroll.start();
           //        router.reload(window.location.pathname)
         }} // If you want to reset the scroll position to 0 for example
@@ -36,13 +34,13 @@ function Layout({children}) {
           }
         }}
       >
-        <main ref={containerRef} className="container" data-scroll-container >
-        <Navigation />
-            {children}
+        <main ref={containerRef} className="container" data-scroll-container>
+          <Navigation />
+          {children}
         </main>
-        </LocomotiveScrollProvider>
-        </>
-    )
+      </LocomotiveScrollProvider>
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
