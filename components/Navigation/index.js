748817e47b182b5258a62cb3e0ad3751dcaed041
Antonio Moccia <moccia.ant@gmail.com>
2022-10-07 14:57:26 +0200

    istemato menu mobile

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,64 +1,78 @@
-import React,{useEffect, useState} from "react";
-import Link from 'next/link'
-import NavLink from './NavLink'
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import NavLink from "./NavLink";
 import Burger from "./Burger";
-import gsap from 'gsap'
-
+import gsap from "gsap";
+import {useLocomotiveScroll} from 'react-locomotive-scroll' 
 function Navigation() {
+  const [open, setOpen] = useState(false);
+  const {scroll,isReady} = useLocomotiveScroll()
 
-const [open,setOpen] = useState(false)
+    useEffect(()=>{
+      const Items =  document.querySelectorAll('.navigation-item')
+      Items.forEach(item=>{
+        item.addEventListener('click',()=>{
+          setOpen(false)
+        })
+      })
+      if(window.innerWidth<=700){
+        document.querySelector('.navigation-wrapper').removeAttribute('data-scroll-section')
+      }else{
 
-useEffect(()=>{
-  window.addEventListener('resize',()=>{
-  setOpen(false)
-  })
-})
+        document.querySelector('.navigation-wrapper').setAttribute('data-scroll-section',true)
+      }
+    },[])
 
-useEffect(()=>{
-  const tl = gsap.timeline()
-  if(open){
-    tl.to('.navigation-list',{
-      x:0
-    }).to('.navigation-item > a',{
-      y:0
-    })
-  }else{
-  tl.to('.navigation-item > a',{
-    y:'120%'
-  }).to('.navigation-list',{
-    x:'-100%'
-  })
-  }
-},[open])
+  useEffect(() => {
+    if(!isReady) return
+    if (window.innerWidth <= 700) {
+  
+      const tl = gsap.timeline();
+      if (open) {
+        tl.to(".burger-wrapper",{
+          rotateZ:0
+        }).to(".navigation-list", {
+          x: 0,
+        }).to(".navigation-item > a", {
+          y: 0,
+        });
+      } else {
+        tl.to(".navigation-item > a", {
+          y: "120%",
+        }).to(".navigation-list", {
+          x: "-100%",
+        }).to(".burger-wrapper",{
+          rotateZ:'-40deg'
+        });
+        
+      }
+    }else{
+
+    }
+  }, [open]);
   return (
-    <div className="navigation-wrapper" >
-      <div className="logo-wrapper"  >
-        <Link href="/" >
-        <img src={'/logo.png'} className="logo" />
+    <div className="navigation-wrapper"  >
+      <div className="logo-wrapper">
+        <Link href="/">
+          <img src={"/logo.png"} className="logo" />
         </Link>
       </div>
       <div className="navigation-container">
         <ul className="navigation-list">
-          <NavLink href='/' >
-                  Home
-            </NavLink>
-            <NavLink href='/chi-sono' >
-                  Chi sono
-            </NavLink>
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="/chi-sono">Chi sono</NavLink>
 
-            <NavLink href="/servizi" >
-                        Servizi
-            </NavLink>
-          <NavLink href="/contatti">
-              Contatti
-          </NavLink>
+          <NavLink href="/servizi">Servizi</NavLink>
+          <NavLink href="/contatti">Contatti</NavLink>
         </ul>
       </div>
-      <Burger onClick={()=>{
-        console.log('clic');
-        setOpen(!open)
-        console.log(open);
-      }} />
+      <Burger
+        onClick={() => {
+          console.log("clic");
+          setOpen(!open);
+          console.log(open);
+        }}
+      />
     </div>
   );
 }
