80c8ebba70e3d671cd277d194a318f1d6fb21d0b
Antonio Moccia <moccia.ant@gmail.com>
2022-10-08 20:37:54 +0200

    link utili

--- pages/contatti.js
+++ pages/contatti.js
@@ -26,15 +26,21 @@ function Contatti({data}) {
             <div className='info-wrapper' >
                 <div className='telefono'>
                     <AiOutlinePhone />
-                {data.phone}
+                    <a href={`tel:${data.phone}`} >
+                    {data.phone}
+                    </a>
                     </div>
                 <div className='address'>
                     <AiOutlinePushpin />
+                    <a target={'_blank'} href='https://www.google.it/maps/place/Via+Nazionale,+3,+83036+Passo+di+Mirabella-pianopantano+AV/@41.0549907,15.0116702,17z/data=!3m1!4b1!4m5!3m4!1s0x133a2834118523d9:0xced982fb05cca6f0!8m2!3d41.0549907!4d15.0138642'>
                         {data.address}
+                    </a>
                     </div>
                 <div className='email'>
                     <AiOutlineMail />
+                      <a href={`mailto:${data.email}`}>
                         {data.email}
+                      </a>
                     </div>
             </div>
         </div>
